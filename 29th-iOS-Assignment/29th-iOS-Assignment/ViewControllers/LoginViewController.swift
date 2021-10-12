6dab4ffd1a1bc7726916db463b7a1d08d02813d0
yangsubinn <cindy010670@gmail.com>
2021-10-13 03:19:10 +0900

    ✅[CHORE] 2차 과제 UI 구성 완료(#4)

--- 29th-iOS-Assignment/29th-iOS-Assignment/ViewControllers/LoginViewController.swift
+++ 29th-iOS-Assignment/29th-iOS-Assignment/ViewControllers/LoginViewController.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 
+import SnapKit
+
 class LoginViewController: UIViewController {
 
     // MARK: - Properties
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
     // MARK: - Life Cycle
@@ -20,21 +26,75 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         configUI()
+        setupLayout()
         setupAddTarget()
         hideKeyboardWhenTappedAround()
     }
     
     // MARK: - Custom Method
+    func configUI() {
+        /// 초기 다음버튼 비활성화
+        nextButton.isEnabled = false
+        
+        logoImageView.image = UIImage(named: "Google")
+        
+        subLabel.text = "Youtube도 이동하면서 계속하세요.\n앱 및 safari에서도 Google 서비스에 로그인됩니다."
+    }
+    
+    func setupLayout() {
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(117)
+            make.height.equalTo(40)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(14)
+            make.centerX.equalToSuperview()
+        }
+        
+        subLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(14)
+            make.centerX.equalToSuperview()
+        }
+        
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(subLabel.snp.bottom).offset(68)
+            make.leading.trailing.equalToSuperview().inset(22)
+            make.height.equalTo(50)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(nameTextField.snp.bottom).offset(17)
+            make.leading.trailing.equalToSuperview().inset(22)
+            make.height.equalTo(50)
+        }
+        
+        pwTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(17)
+            make.leading.trailing.equalToSuperview().inset(22)
+            make.height.equalTo(50)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.top.equalTo(pwTextField.snp.bottom).offset(73)
+            make.leading.equalToSuperview().inset(22)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.centerY.equalTo(signUpButton.snp.centerY)
+            make.trailing.equalToSuperview().inset(22)
+            make.width.equalTo(74)
+            make.height.equalTo(50)
+        }
+    }
+    
     func setupAddTarget() {
         /// 요소에 따라 정의해둔 함수를 실행
         nameTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
         emailTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
         pwTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
-    }
-    
-    func configUI() {
-        /// 초기 다음버튼 비활성화
-        nextButton.isEnabled = false
     }
     
     // MARK: - @IBAction
--- 29th-iOS-Assignment/29th-iOS-Assignment/ViewControllers/SuccessViewController.swift
+++ 29th-iOS-Assignment/29th-iOS-Assignment/ViewControllers/SuccessViewController.swift
@@ -10,17 +10,52 @@ import UIKit
 class SuccessViewController: UIViewController {
 
     // MARK: - Properties
+    @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var okayButton: UIButton!
+    @IBOutlet weak var anotherAccountButton: UIButton!
     var message: String?
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupLayout()
+        configUI()
         setNameLabel()
     }
     
     // MARK: - Custom Method
+    func configUI() {
+        anotherAccountButton.setTitle("다른 계정으로 로그인하기", for: .normal)
+        logoImageView.image = UIImage(named: "Google")
+    }
+    
+    func setupLayout() {
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(207)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(117)
+            make.height.equalTo(40)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(14)
+            make.centerX.equalToSuperview()
+        }
+        
+        okayButton.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(53)
+            make.leading.trailing.equalToSuperview().inset(22)
+            make.height.equalTo(50)
+        }
+        
+        anotherAccountButton.snp.makeConstraints { make in
+            make.top.equalTo(okayButton.snp.bottom).offset(23)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
     /// 받아온 이름 넣는 함수
     func setNameLabel() {
         if let name = message {
