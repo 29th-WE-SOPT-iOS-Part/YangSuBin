6dd1b7811855e5fd5781ddeda4b7785d7f8eee1a
yangsubinn <cindy010670@gmail.com>
2021-10-13 01:25:27 +0900

    ✅[CHORE] #1 코드리뷰 반영 수정(#3)

--- 29th-iOS-Assignment/29th-iOS-Assignment/ViewControllers/SignupViewController.swift
+++ 29th-iOS-Assignment/29th-iOS-Assignment/ViewControllers/SignupViewController.swift
@@ -9,26 +9,36 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    // MARK: - Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var showPwButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 초기 다음버튼 비활성화
-        nextButton.isEnabled = false
-        
-        // 요소에 따라 정의해둔 함수를 실행
+        configUI()
+        setupAddTarget()
+    }
+    
+    // MARK: - Custom Method
+    func setupAddTarget() {
+        /// 요소에 따라 정의해둔 함수를 실행
         nameTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
         emailTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
         pwTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
         showPwButton.addTarget(self, action: #selector(showPwButtonClicked(button:)), for: .touchUpInside)
-
     }
     
+    func configUI() {
+        /// 초기 다음버튼 비활성화
+        nextButton.isEnabled = false
+    }
+    
+    // MARK: - @IBAction
     @IBAction func touchUpToSuccess(_ sender: Any) {
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SuccessViewController") as? SuccessViewController else {return}
         
@@ -37,17 +47,20 @@ class SignupViewController: UIViewController {
         self.present(nextVC, animated: true, completion: nil)
     }
     
-    // textField 변화에 따라 수행할 함수
+    // MARK: - @objc
+    /// textField 변화에 따라 수행할 함수
     @objc
     func textFieldDidChange(textField: UITextField) {
-        if self.nameTextField.hasText && self.emailTextField.hasText && self.pwTextField.hasText {
-            nextButton.isEnabled = true
-        } else {
-            nextButton.isEnabled = false
-        }
+        pwTextField.isSecureTextEntry = pwTextField.isSecureTextEntry == true ? false : true
+        showPwButton.tintColor = pwTextField.isSecureTextEntry == true ? .systemBlue : .lightGray
+//        if self.nameTextField.hasText && self.emailTextField.hasText && self.pwTextField.hasText {
+//            nextButton.isEnabled = true
+//        } else {
+//            nextButton.isEnabled = false
+//        }
     }
     
-    // 비밀번호 표시 버튼에 따라 수행할 함수
+    /// 비밀번호 표시 버튼에 따라 수행할 함수
     @objc
     func showPwButtonClicked(button: UIButton) {
         if pwTextField.isSecureTextEntry == true {
@@ -58,5 +71,4 @@ class SignupViewController: UIViewController {
             showPwButton.tintColor = .lightGray
         }
     }
-    
 }
