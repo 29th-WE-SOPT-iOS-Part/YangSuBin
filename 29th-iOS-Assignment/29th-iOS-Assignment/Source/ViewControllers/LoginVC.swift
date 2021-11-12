f5369a14ff7e769ae29d3d2e59f6b013b9562785
yangsubinn <cindy010670@gmail.com>
2021-11-12 15:56:00 +0900

    [Del] 불필요한 코드 삭제(#12)

--- 29th-iOS-Assignment/29th-iOS-Assignment/Source/ViewControllers/LoginVC.swift
+++ 29th-iOS-Assignment/29th-iOS-Assignment/Source/ViewControllers/LoginVC.swift
@@ -48,23 +48,6 @@ class LoginVC: UIViewController {
         pwTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
     }
     
-//    func showAlert(title: String, message: String, okAction: ((UIAlertAction) -> Void)? = nil) {
-//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-//        let okAction = UIAlertAction(title: "확인", style: .default, handler: { action in
-//            /// 서버에서 받은 message가 "로그인 성공"일 때만 화면 전환
-//            if message == "로그인 성공" {
-//                /// present 화면 전환
-//                /// Main 스토리보드의 LoginVC -> Main 스토리보드의 SuccessVC
-//                guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SuccessVC") as? SuccessVC else {return}
-//
-//                nextVC.modalPresentationStyle = .fullScreen
-//                self.present(nextVC, animated: true, completion: nil)
-//            }
-//        })
-//        alert.addAction(okAction)
-//        present(alert, animated: true)
-//    }
-    
     // MARK: - @IBAction
     @IBAction func touchUpToSuccess(_ sender: Any) {
         requestLogin()
--- 29th-iOS-Assignment/29th-iOS-Assignment/Source/ViewControllers/SignupVC.swift
+++ 29th-iOS-Assignment/29th-iOS-Assignment/Source/ViewControllers/SignupVC.swift
@@ -46,23 +46,6 @@ class SignupVC: UIViewController {
         showPwButton.addTarget(self, action: #selector(showPwButtonClicked(button:)), for: .touchUpInside)
     }
     
-//    func showAlert(title: String, message: String, okAction: ((UIAlertAction) -> Void)? = nil) {
-//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-//        let okAction = UIAlertAction(title: "확인", style: .default, handler: { action in
-//            /// 서버에서 받은 message가 "회원 가입 성공"일 때만 화면 전환
-//            if message == "회원 가입 성공" {
-//                /// present 화면 전환
-//                /// Main 스토리보드의 SignupVC -> Main 스토리보드의 SuccessVC
-//                guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SuccessVC") as? SuccessVC else {return}
-//
-//                nextVC.modalPresentationStyle = .fullScreen
-//                self.present(nextVC, animated: true, completion: nil)
-//            }
-//        })
-//        alert.addAction(okAction)
-//        present(alert, animated: true)
-//    }
-    
     // MARK: - @IBAction
     @IBAction func touchUpToSuccess(_ sender: Any) {
         requestSignup()
